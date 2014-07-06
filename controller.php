aa3037bddaeae43a02cf302500f63e18bc891d91
Jim Mello <jmello@g.harvard.edu>
2014-07-05 21:29:38 -0400

    basic logic completed

--- controller.php
+++ controller.php
@@ -1,4 +1,16 @@
 <?php 
 
 $num_words = $_POST['num_words'];
-$result = $num_words;
+$result = "";
+
+$dictionary = Array("cat", "dog", "horse", "rabbit", "koala", "elephant", "sloth", "jaguar", "raccoon", "turtle", "lion");
+
+for ($i = 0; $i < $num_words; $i++) {
+	$word = $dictionary[rand(0, count($dictionary)-1)];
+	if ($i == 0) {
+		$result = $word;
+	}
+	else {
+		$result = $result . "-" . $word;
+	}
+}
--- index.php
+++ index.php
@@ -3,12 +3,28 @@
 	<head>
 	<meta charset="utf-8"/>
 	<title>Password Generator</title>
+	
 	<?php require 'controller.php'; ?>
+	
+	<script>
+		function validate_num (id) {
+			/* alert("1"); */
+			var words = getElementById("num_words").value;
+			alert(words);
+			if (words >= 1 && words <= 9) {
+				return;
+			}
+			else {
+				alert("Please enter a number between 1 and 9!");
+			}
+		}
+	</script>
 	</head>
+	
 	<body>
 		<h1>xkcd Password Generator</h1>
 		<form action="index.php" method="POST">
-			<input type="input" name="num_words" />
+			<input type="input" name="num_words" id="num_words" onchange="validate_num(this)" />
 			<input type="submit" />
 		</form>
 		</br>
