96b6c833c64b605a8d2844faa1361268ccd6b487
Jim Mello <jmello@g.harvard.edu>
2014-07-05 21:56:17 -0400

    added additional options for symbols, numbers, and upper/lower casing

--- controller.php
+++ controller.php
@@ -1,6 +1,10 @@
 <?php 
 
+//print_r( $_POST);
 $num_words = $_POST['num_words'];
+$inc_symbol = $_POST['inc_symbol'];
+$inc_number = $_POST['inc_number'];
+$to_case = $_POST['to_case'];
 $result = "";
 
 $dictionary = Array("cat", "dog", "horse", "rabbit", "koala", "elephant", "sloth", "jaguar", "raccoon", "turtle", "lion");
@@ -13,4 +17,19 @@ for ($i = 0; $i < $num_words; $i++) {
 	else {
 		$result = $result . "-" . $word;
 	}
+}
+
+if ($to_case == "upper"){
+	$result = strtoupper($result);
+}
+else if ($to_case == "lower") {
+	$result = strtolower($result);
+}
+
+if($inc_number == "on") {
+	$result = $result . "-@";
+}
+
+if ($inc_symbol == "on") {
+	$result = $result . "-4";
 }
--- index.php
+++ index.php
@@ -24,10 +24,14 @@
 	<body>
 		<h1>xkcd Password Generator</h1>
 		<form action="index.php" method="POST">
-			<input type="input" name="num_words" id="num_words" onchange="validate_num(this)" />
-			<input type="submit" />
+			<input type="input" name="num_words" id="num_words" onchange="validate_num(this)" /><br/>
+			<input type="checkbox" name="inc_number" id="inc_number">Include a number</input><br/>
+			<input type="checkbox" name="inc_symbol" id="inc_symbol">Include a symbol</input><br/>
+			<input type="radio" name="to_case" id="to_case" value="upper">All uppercase</input>
+			<input type="radio" name="to_case" id="to_case" value="lower">All lowercase</input><br/>
+			<input type="submit" /><br/>
 		</form>
 		</br>
-		Password = <?php echo $result; ?>
+		<span> Password = <?php echo $result; ?> </span>
 	</body>
 </html>
