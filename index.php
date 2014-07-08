63987eebf422b336864ada2aca57eb61b5655ad5
Jim Mello <jmello@g.harvard.edu>
2014-07-08 12:52:48 -0400

    Updated formatting, added description of an xkcd generator

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 	<?php require 'controller.php'; ?>
 	
 	<script>
+		/*  Validate that the input from the text box was a number from 1 to 9, 
+			and do not enable the submit button unless it is  */
 		function validate_num (id) {
 			var numwords = document.getElementById("num_words").value;
 			var subButton = document.getElementById("submit_button");
@@ -25,8 +27,14 @@
 	
 	<body>
 		<form action="index.php" method="POST">
-			<div style="text-align:center; width:100%; font-size:20pt">
+			<div style="text-align:center; width:100%; font-size:16pt">
 				<h1>xkcd Password Generator</h1>
+				<p>
+				An xkcd password generator generates a password using common words which are easier to remember than more esoteric forms used for passwords.
+				This site will generate an xkcd style password of varying length, and gives you the option of uppercasing, lowercasing, or camelcasing the words.
+				You may also add a randomly chosen number or punctuation symbol to the end of the password.<br/>
+				</p>
+				<p>
 				Number of Words (1-9):<input type="input" name="num_words" id="num_words" size="4" onchange="validate_num(this)" value="<?php echo $num_words ?>"></input><br/>
 				<input type="checkbox" name="inc_number" id="inc_number" <?php echo ($inc_number == "on") ? "checked" : ""; ?> >Include a Number</input><br/>
 				<input type="checkbox" name="inc_symbol" id="inc_symbol" <?php echo ($inc_symbol == "on") ? "checked" : ""; ?> >Include a Symbol</input><br/>
@@ -36,6 +44,7 @@
 				<input type="submit" id="submit_button" value="Get Password" /><br/>
 				</br>
 				<?php echo ($result != "") ? "Password = $result" : ""; ?>
+				</p>
 			</div>
 		</form>
 	</body>
